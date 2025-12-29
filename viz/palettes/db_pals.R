297feb781d9389ef90529ab0e51965dcad12567b
David Bacsik <dbacsik@uw.edu>
2025-12-29 12:08:14 -0800

    Updated display notebooks

---
+++ viz/palettes/db_pals.R
@@ -0,0 +1,14 @@
+pal_okabe_ito <- c("#E69F00", "#56B4E9", "#009E73", "#F5C710", "#0072B2", "#D55E00", "#CC79A7", "#999999")
+
+pal_bacsik_bright <- c("#54d2d2", "#ff6150", "#ffcd00", "#33c080", "#9d50ac")
+
+pal_clinical_modern <- c("#F4D58D", "#8DB4D2", "#958ED2", "#D2958E", "#8ED2C2")
+
+db_pals <- list(
+  okabe_ito = pal_okabe_ito,
+  bacsik_bright = pal_bacsik_bright,
+  clinical_modern = pal_clinical_modern
+)
+
+# Confirmation message
+message("Loaded personal palettes from db_pals.R. Type `db_pals` to see a list of available options.")
